df6ffeaf8c5e35a55ff45b5ca6ad512dc64d1d55
Christoph Scheffauer <scheffauerc@gmail.com>
2020-01-03 00:23:38 +0100

    built getMorePeople function button to load more still needed

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,35 @@
 export const apiCall = (link) => {
-
-    return fetch(link)
+    let people = [];
+    people = fetch(link)
         .then(data => data.json())
         .then(data => data.results)
+        //.then(data => getMorePeople(data.results, data.next))
+        .then(people => getSpecies(people))
+    return people;
+}
+
+async function getMorePeople(people, link) {
+    let tempPeople = [];
+    let nextLink = undefined;
+    for (let i = 0; i < 6; i++) {
+        link = (nextLink === undefined) ? link : nextLink;
+        let data = await fetch(link);
+        let data_json = await data.json();
+        let morePeople = data_json.results;
+
+        nextLink = await data_json.next;
+        tempPeople = [...tempPeople, ...morePeople];
+    }
+    return tempPeople;
+}
+
+
+function getSpecies(people) {
+    people.map((person, i) => {
+        fetch(person.species[0])
+            .then(data => data.json())
+            .then(data => { people[i].species = data.name })
+        return person;
+    })
+    return people;
 }
