077b6ca18e50b7df91a6970e5a4285df30fc86a8
Christoph Scheffauer <scheffauerc@gmail.com>
2020-01-18 23:43:36 +0100

    Updates

---
+++ precache-manifest.499b6740ad50304357f34900dc60d620.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "220eb0d16d2bdb55506590159f52be29",
+    "url": "/starwars/index.html"
+  },
+  {
+    "revision": "b839771df8a29a69f237",
+    "url": "/starwars/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "64d49d7f2e2eeb52761b",
+    "url": "/starwars/static/css/3.be156923.chunk.css"
+  },
+  {
+    "revision": "e54ef1825af57b9323cb",
+    "url": "/starwars/static/css/4.5627a905.chunk.css"
+  },
+  {
+    "revision": "a0eda37afcd1bba268cb",
+    "url": "/starwars/static/css/main.c6f821ef.chunk.css"
+  },
+  {
+    "revision": "b839771df8a29a69f237",
+    "url": "/starwars/static/js/2.d6418dad.chunk.js"
+  },
+  {
+    "revision": "64d49d7f2e2eeb52761b",
+    "url": "/starwars/static/js/3.5be8db3c.chunk.js"
+  },
+  {
+    "revision": "e54ef1825af57b9323cb",
+    "url": "/starwars/static/js/4.50198277.chunk.js"
+  },
+  {
+    "revision": "a0eda37afcd1bba268cb",
+    "url": "/starwars/static/js/main.d1ff591b.chunk.js"
+  },
+  {
+    "revision": "c7a76098f07f1732c9dd",
+    "url": "/starwars/static/js/runtime~main.759bf9dc.js"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/starwars/static/media/Starjedi.cc3019aa.ttf"
+  }
+]);
