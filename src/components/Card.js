bb28bdb1b4785413aa7b4f0ad0355b291ee3c925
Christoph Scheffauer <scheffauerc@gmail.com>
2020-04-25 19:44:13 +0200

    Card adjustments

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,9 +9,30 @@ const Card = ({ id, name, image, types, }) => {         //receiving props and de
         <Fragment>
             <div className='card'>
                 <img alt={`image ` + name} src={image} />
-                <div>
-                    <h2>{name}</h2>
-                    <p>Types: {types}</p>
+                <div className='pokemonNameSection'>
+                    <p>{name}</p>
+                </div>
+                <div className='pokemonTypeSection'>
+                    <div className='pokemonTypeOverlay'>
+                        <div className='pokemonTypeOverlayBackground' style={{ backgroundImage: `url('/images/textures/${types[0]}.png')` }} />
+                    </div>
+
+                    {
+                        types.length === 2 ?
+                            <>
+                                <div className='pokemonTypePositionOne pokemonType'>
+                                    <p>{types[0]}</p>
+                                </div>
+                                <div className='pokemonTypePositionTwo pokemonType'>
+                                    <p>{types[1]}</p>
+                                </div>
+                            </>
+                            :
+                            <div className='pokemonTypePosition pokemonType'>
+                                <p>{types}</p>
+                            </div>
+                    }
+                    <div className='trianglePosition pokemonClosedTriangle'></div>
                 </div>
             </div>
         </Fragment>
